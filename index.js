18c04414434a37c8b93ae689f30a14daeb5ac94e
mac <mac@MacBook-Pro-mac.local>
2023-01-20 15:43:51 +0300

    start

--- index.js
+++ index.js
@@ -3,8 +3,13 @@ import express from "express";
 const app = express();
 const PORT = process.env.PORT || 2107;
 
-app.get("/", (req, res) => {
-  res.status(200).json("Сервер работает");
+app.get("/api", (req, res) => {
+  // res.status(200).json("Сервер работает");
+  res.send("Hello World, from express");
 });
 
 app.listen(PORT, () => console.log("SERVER STARTED IN PORT :" + PORT));
+
+// fetch("https://polydisk.onrender.com/")
+//   .then((res) => console.log(res.body))
+//   .catch((er) => console.log(er.message, "errrrrrororfodfndnjf"));
