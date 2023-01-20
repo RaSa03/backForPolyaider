a65b7a407833db84366f23e1e6d731e583a18b0b
mac <mac@MacBook-Pro-mac.local>
2023-01-20 16:28:41 +0300

    start2

--- index.js
+++ index.js
@@ -1,15 +1,24 @@
 import express from "express";
+import metaPkg from "ya-disk";
 
+const { meta } = metaPkg;
 const app = express();
+const API_TOKEN = "y0_AgAAAABlkAHfAADLWwAAAADaETNUcJOnM6c7SheCwSzNvgfylNrtUB4";
 const PORT = process.env.PORT || 2107;
 
 app.get("/api", (req, res) => {
-  // res.status(200).json("Сервер работает");
-  res.send("Hello World, from express");
+  console.log(req.url, req.method);
+  (async () => {
+    try {
+      const metaGet = await meta.get(API_TOKEN, "Новая папка");
+      return metaGet;
+    } catch (error) {
+      console.error("errrrorr", error);
+    }
+  })().then((meta) => {
+    const json = JSON.stringify(meta);
+    res.send(json);
+  });
 });
 
 app.listen(PORT, () => console.log("SERVER STARTED IN PORT :" + PORT));
-
-// fetch("https://polydisk.onrender.com/")
-//   .then((res) => console.log(res.body))
-//   .catch((er) => console.log(er.message, "errrrrrororfodfndnjf"));
