457650ce15e70166c8577ec770dd27be0eee0a6d
mac <mac@MacBook-Pro-mac.local>
2023-01-22 17:13:15 +0300

    complete

--- index.js
+++ index.js
@@ -1,24 +1,54 @@
 import express from "express";
 import metaPkg from "ya-disk";
+import path from "path";
+import dotenv from "dotenv";
+dotenv.config();
 
 const { meta } = metaPkg;
 const app = express();
-const API_TOKEN = "y0_AgAAAABlkAHfAADLWwAAAADaETNUcJOnM6c7SheCwSzNvgfylNrtUB4";
+const API_TOKEN = process.env.YaToken;
 const PORT = process.env.PORT || 2107;
 
-app.get("/api", (req, res) => {
-  console.log(req.url, req.method);
-  (async () => {
-    try {
-      const metaGet = await meta.get(API_TOKEN, "Новая папка");
-      return metaGet;
-    } catch (error) {
-      console.error("errrrorr", error);
-    }
-  })().then((meta) => {
-    const json = JSON.stringify(meta);
-    res.send(json);
-  });
-});
+const normalizePAth = (url) => {
+  try {
+    url = decodeURI(url);
+  } catch (e) {
+    console.error(e);
+  }
+  let path = url.slice(5).split("%2F").join("/");
+  return path;
+};
+app
+  .use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "http://localhost:5173");
+    res.setHeader(
+      "Access-Control-Allow-Methods",
+      "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+    );
+    res.setHeader(
+      "Access-Control-Allow-Headers",
+      "X-Requested-With,content-type"
+    );
+    res.setHeader("Access-Control-Allow-Credentials", true);
+    next();
+  })
 
-app.listen(PORT, () => console.log("SERVER STARTED IN PORT :" + PORT));
+  .get("/api/:path", (req, res) => {
+    const PATH = normalizePAth(req.params.path);
+    console.log(PATH);
+    (async () => {
+      try {
+        const metaGet = await meta.get(API_TOKEN, PATH, {
+          limit: 1000,
+        });
+        return metaGet;
+      } catch (error) {
+        console.error("errrrorr", error);
+      }
+    })().then((meta) => {
+      const json = JSON.stringify(meta);
+      res.send(json);
+    });
+  })
+
+  .listen(PORT, () => console.log("SERVER STARTED IN PORT :" + PORT));
